2b2d4f99046cb89fc3f3807abf748d742226dc1b
JamiyanD <115760624+JamiyanD@users.noreply.github.com>
2022-12-04 22:29:31 +0800

    12.04 at home

--- day-13/js/ex1.js
+++ day-13/js/ex1.js
@@ -9,31 +9,24 @@ while (count < 10) {
 // program to check if a number is prime or not
 
 // take input from the user
-/*const number = parseInt(prompt("Enter a positive number: "));
-let isPrime = true;
+// const number = prompt("Enter a positive number: ");
+// let isPrime = true;
+// // check if number is greater than 1
+//  if (number > 1) {
 
-// check if number is equal to 1
-if (number === 1) {
-    console.log("1 is neither prime nor composite number.");
-}
+//     // looping through 2 to number-1
+//     for (let i = 2; i < number; i++) {
+//         if (number % i == 0) {
+//             isPrime = false;
+//         }
+//     }
 
-// check if number is greater than 1
-else if (number > 1) {
-
-    // looping through 2 to number-1
-    for (let i = 2; i < number; i++) {
-        if (number % i == 0) {
-            isPrime = false;
-            break;
-        }
-    }
-
-    if (isPrime) {
-        console.log(`${number} is a prime number`);
-    } else {
-        console.log(`${number} is a not prime number`);
-    }
-}*/
+//     if (isPrime) {
+//         console.log(`${number} is a prime number`);
+//     } else {
+//         console.log(`${number} is a not prime number`);
+//     }
+// }
 /*let a = Number(prompt("duriin too oruulna uu"));
 let count = 0;
 let sum = 0;
@@ -44,29 +37,65 @@ while(count < a){
     console.log(sum);
 }*/
 
-/*let a = Number(prompt("duriin too oruulna uu"));
-let count = 0;
-let urjver = 1;
-while(count < a){
-    count = count + 1;
-    urjver = count * urjver;
-    console.log(urjver);
-}*/
+// let a = Number(prompt("duriin too oruulna uu"));
+// let count = 0;
+// let urjver = 1;
+// while(count < a){
+//     count = count + 1;
+//     urjver = count * urjver;
+
+// }
+// console.log(urjver);
 
 //let a = Number(prompt("duriin too oruulna uu"));
 //let b = Math.ceil(Math.random()*100) ;
 
-console.log("     *")
-count = "*";
-a = "    *";
-b = "*";
-while(count < "***** " && a < "    *****"){
+// let x = 5;
+// let b ="     *"
+// count = "";
+
+// while(count.length <= x ){
    
  
-    console.log(a + count + b);
-    count = count + "*"; 
-    a = a + "*";
-    b = b + "*";
-}
+    
+//     count = count + "*"; 
+//     console.log(b + count);
+    
+// }
+var pyramid;
+// var n = 5;
+// for (var i = 1; i <= n; i++) {
+//     var s = "";
+//     // For every each counter there exist 2*n-1 value
+//     for (var j = 1; j <= (2 * n - 1); j++) {
+//       // Hint: Check the workbook image
+//       if(j >= n + 1 - i && j <= n - 1 + i){
+//         s = s + "*";
+//       }else{
+//         s = s + " "
+       
+//       } 
+  
+//     }
+//     console.log(s);
+// }
 
+let square;
+let n = 5;
+// for(let i = 1; i <= n; i++){
+//     let s = "";
+//     if(i == 1){
+//         s = "*****"
+//     }else if(i == 2){
+//         s ="*   *"
+//     }else if(i == 3){
+//         s ="*   *"
+//     }else if(i == 4){
+//         s ="*   *"
+//     }else if(i == n){
+//         s ="*****"
+//     }
+//     console.log(s);
+    
+// }
 
--- day-14/js/assignments.js
+++ day-14/js/assignments.js
@@ -10,7 +10,7 @@
 //     console.log(sum);
 // }
 
-
+// let str = "123";
 // for(count = 0;count < str.length; count++){
 //     sum = str.charAt(count);
 //     console.log(sum);
@@ -26,12 +26,130 @@
 // }
 // console.log(sum);
 
-let N = Number(prompt("give me number"));
+// let N = Number(prompt("give me number"));
 
-for(sum = 0, rem = 0; N > 0; ){
-    rem = N % 10
-     sum = sum + rem
-      N = Math.floor(N / 10)
+// for(sum = 0, rem = 0; N > 0; ){
+//     rem = N % 10
+//      sum = sum + rem
+//       N = Math.floor(N / 10)
 
-}
-console.log(sum);
+// }
+// console.log(sum);
+
+// let limit = prompt("duriin too oruulna uu");
+// let count = 0;
+// let sum = 0;
+// while(count <= limit){
+//     if(count % 2 !== 0){
+//         sum = count + sum;
+//         console.log(sum);
+//     }
+//     count = count + 1;
+// }
+
+// let a = "289";
+// let ex = "";
+// for(count = a.length - 1; count >= 0; count--){
+//     ex = ex + a.charAt(count);
+// }
+// console.log(ex);
+
+
+// let a = prompt("duriin baga too ogno u");
+// let b = prompt("duriin ih too ogno uu");
+
+// for(number = a; number <= b; number++){
+   
+
+// let isPrime = true;
+
+//  if (number >= 1) {
+
+  
+//     for (let i = 2; i < number; i++) {
+//         if (number % i == 0) {
+//             isPrime = false;
+//         }
+//     }
+//     if (isPrime) {
+//         console.log(number);
+//     } 
+
+// }
+
+// }
+
+// let a = prompt("Ta hussen neg toogoo oruulna uu");
+// let count = 0;
+
+
+// while(a !== 0){
+//     count  = count + 1;
+//     a = Math.floor(a / 10);
+// }
+// console.log(count);
+
+// var number = Number(prompt("100 dotor hussen N too ogno uu"));
+// if(number < 100){
+// var x = 0;
+// while(x !== number){
+//   x = Math.floor((Math.random() * 100) + 1);
+//   console.log(x);
+// }
+// } else{
+//     console.log("ogson too buruu bna ");
+// }
+
+// var a = "This is STRING";
+// var b = "";
+
+// for(count = 0; count < a.length; count++){
+    
+//     if(a.charAt(count) === a.charAt(count).toUpperCase()){
+//         b = b + a.charAt(count).toLowerCase() ;
+        
+//      }else{
+//           b = b + a.charAt(count).toUpperCase() ;
+     
+//      }
+// }
+// console.log(b);
+
+let toonii_urvuu;
+// let a = "289";
+// let ex = "";
+// for(count = a.length - 1; count >= 0; count--){
+//     ex = ex + a.charAt(count);
+// }
+// console.log(ex);
+
+let fibonachiin_daraalal;
+// let a = Number(prompt("duriin N too oruulna uu"));
+// let num1 = 0;
+// let num2 = 1;
+// let sum = 0;
+// for(i = 0; i < a; i++){
+//     sum = num1 + num2;
+//     num1 = num2;
+//     num2 = sum;
+//     // console.log(num1);
+//     if(num1 < a){
+//         console.log(num1);
+//     }
+// }
+
+let palindromic_number;
+
+// let a = prompt("dsada");
+// let ex = "";
+// for(count = a.length - 1; count >= 0; count--){
+//     ex = ex + a.charAt(count);
+// }
+
+
+// if(ex == a){
+//     console.log("true");
+// }else{
+//     console.log("false");
+// }
+
