4036fef37979478ba036ce970d8a456edbccbf95
JamiyanD <115760624+JamiyanD@users.noreply.github.com>
2022-12-14 22:38:26 +0800

    12.14

--- JS-DOM/day-20/js/events.js
+++ JS-DOM/day-20/js/events.js
@@ -1,14 +1,7 @@
 let myButton = document.getElementById("myButton");
-console.log(myButton);
 let color = "green";
-function changeBackgroundColorGreen () {
-    myButton.style = "background-color: green"
-}
-function changeBackgroundColorWhite () {
-    myButton.style = "background-color: white"
-}
+
 function changeColors(){
-    console.log("clicked");
     if(color === "green"){ 
         myButton.style = `background-color: ${color}`;
         color = "white";
@@ -17,17 +10,8 @@ function changeColors(){
         color = "green";
     }
 }
-
 myButton.addEventListener("click", changeColors);
 
-
-let arrowFunc = () => {
-
-}
-
-myButton.addEventListener("click", () => {   // arrow function-g shuud enchenen bichij bolno
-    myButton.style = "background-color: red;"
-})
 
 
 let registerButton = document.querySelector("#register");
@@ -35,11 +19,10 @@ registerButton.disabled = true; // buttong ajillahgui bolgodog
 
 let button = document.getElementById("yourButton");
 let p1 = document.getElementById("haha");
-console.log(p1);
 let p2 = document.getElementById("hehe");
 
 function pFunc(){
-    document.getElementById("haha")
     p2.style = "background-color: yellow;"
+    p1.style = "background-color: yellow;"
 }
 button.addEventListener("click", pFunc)
